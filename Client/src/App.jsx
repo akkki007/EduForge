3db8331662c683dc066b63432377589b45c967bd
Akshay Nazare <akshaynazare3@gmail.com>
2025-01-17 09:06:46 +0530

    Template added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Home from './components/Home';
 // import About from './components/About';
 // import Contact from './components/Contact';
-import NotFound from "./components/NotFound";
+import Home from "./Home";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route exact path="/" element={<Home />} />
+        {/*<Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
